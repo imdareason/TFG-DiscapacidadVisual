1d1d43a0b0474214f85d79e09d367a91de825204
Erik <erikagujari@gmail.com>
2017-05-26 19:20:04 +0200

    Added permissions control.

--- app/src/main/java/com/gettingstarted/erik/tfg_appdiscvisual/MainActivity_show_camera.java
+++ app/src/main/java/com/gettingstarted/erik/tfg_appdiscvisual/MainActivity_show_camera.java
@@ -1,7 +1,11 @@
 package com.gettingstarted.erik.tfg_appdiscvisual;
 
 //Android Classes
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
@@ -59,6 +63,7 @@ public class MainActivity_show_camera extends AppCompatActivity implements CvCam
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.checkPermissions();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.show_camera);
@@ -102,6 +107,31 @@ public class MainActivity_show_camera extends AppCompatActivity implements CvCam
             mOpenCvCameraView.disableView();
     }
 
+    private void checkPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+
+        }
+    }
     public void onCameraViewStarted(int width, int height ){
         mRgba = new Mat(height,width,CvType.CV_8U);
         mGrayScale = new Mat(height,width,CvType.CV_8U);
